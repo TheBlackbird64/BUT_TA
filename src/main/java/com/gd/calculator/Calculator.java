678df56708ccde827e0acf2df797633d8d98b8ad
Gabriel Dunogue <gabriel.dunogue@gmail.com>
2026-09-23 15:02:48 +0200

    added add and divide methods and tests

---
+++ src/main/java/com/gd/calculator/Calculator.java
@@ -0,0 +1,12 @@
+package com.gd.calculator;
+
+public class Calculator {
+
+    public int add(int opG, int opD) {
+        return opG + opD;
+    }
+
+    public int divide(int opG, int opD) {
+        return opG / opD;
+    }
+}
---
+++ src/test/java/com/gd/calculator/CalculatorTest.java
@@ -0,0 +1,36 @@
+package com.gd.calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CalculatorTest {
+
+    private final Calculator calculator = new Calculator();
+
+    @Test
+    void add_devrait_calculer_la_somme_de_deux_int() {
+        // GIVEN
+        int opG = 2;
+        int opD = 3;
+
+        // WHEN
+        int resultat = calculator.add(opG, opD);
+
+        // THEN
+        assertThat(resultat).isEqualTo(5);
+    }
+
+    @Test
+    void divide_devrait_calculer_le_quotient_de_deux_int() {
+        // GIVEN
+        int opG = 10;
+        int opD = 2;
+
+        // WHEN
+        int resultat = calculator.divide(opG, opD);
+
+        // THEN
+        assertThat(resultat).isEqualTo(5);
+    }
+}
